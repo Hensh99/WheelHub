a292f7c1696e21d82f8f906dbc80280ab468f504
Mohamed Hany <mohamedhany.mh.1999@gmail.com>
2024-04-07 05:40:13 +0200

    Data Serialization: Interceptors

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,12 +8,17 @@ import {
   Param,
   Query,
   NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
